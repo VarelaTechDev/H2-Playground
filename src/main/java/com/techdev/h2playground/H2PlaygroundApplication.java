5b52b11812027b4110ddf894240dcae0d1b10b37
VarelaTechDev <varela.tech.dev@gmail.com>
2023-07-31 20:46:40 -0400

    Initial commit

---
+++ src/main/java/com/techdev/h2playground/H2PlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.techdev.h2playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2PlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2PlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techdev/h2playground/H2PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techdev.h2playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
